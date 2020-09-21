f76c935bb918bd48e1055004cd3805cde45f5c66
julioct99 <jcasallot01@gmail.com>
2020-09-21 18:57:22 +0200

    API search

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="background">
+    <div class="container mb-5">
+      <form
+        @submit.prevent="loadIds"
+        class="mb-5"
+      >
+        <input
+          type="text"
+          v-model="query"
+        >
+      </form>
+      <div class="row card-group">
+        <Card
+          v-for="item in items"
+          :key="item.imdbID"
+          :item="item"
+        ></Card>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Card from './components/Card.vue'
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      ids: [],
+      items: [],
+      query: 'star'
+    }
+  },
+  components: {
+    Card
+  },
+  methods: {
+    loadIds() {
+      console.log(this.query)
+      this.ids = []
+      this.items = []
+      let page = 1
+      while (page < 4) {
+        axios
+          .get(`http://www.omdbapi.com/?s=${this.query}&apikey=thewdb&page=${page}`)
+          .then(res => {
+            const items = res.data.Search
+            if (items) {
+              items.forEach((item, index) => {
+                this.loadItem(item.imdbID)
+                this.ids.push(item.imdbID)
+              })
+            } else if (page === 1) {
+              // showMessage('No results found')
+            }
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        page++
+      }
+    },
+    loadItem(id) {
+      axios
+        .get(`http://www.omdbapi.com/?i=${id}&apikey=thewdb`)
+        .then(res => {
+          const item = res.data
+          this.items.push(item)
+        })
+        .catch(err => console.log(err))
+    }
+  },
+  created() {
+    this.loadIds(this.query)
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#background {
+  background: #232526;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="col-lg-4 col-sm-6 mb-4">
+    <div class="card bg-dark mb-3">
+      <img
+        :src="item.Poster"
+        class="card-img-top"
+        alt="Poster"
+      >
+      <div class="card-body">
+        <a :href="imdbLink">
+          <h5 class="card-title"> {{item.Title}} <span class="item-year"> (YEAR)</span></h5>
+        </a>
+        <p>N IMDB votes</p>
+        <div class="d-flex justify-content-center align-items-center text-center">
+          <div class="d-flex flex-column">
+            <div class="row align-items-center mb-2">
+              <div class="col">
+                <img
+                  class="icon"
+                  src=""
+                  alt="Score logo"
+                >
+              </div>
+              <div class="col">
+                <span class="score">5</span>
+              </div>
+            </div>
+          </div>
+          <!-- ${mcHTML}
+          ${rtHTML} -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    imdbLink() {
+      return `https://www.imdb.com/title/${this.item.imdbID}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  font-family: "Roboto", sans-serif;
+}
+
+h5 {
+  font-weight: bolder;
+}
+
+a {
+  color: #00a2ff;
+}
+
+i,
+p,
+.score {
+  color: white;
+}
+
+a:hover {
+  text-decoration: none;
+  transition: 0.2s ease-in-out;
+  color: #fce205;
+  text-shadow: 0px 0px 5px black;
+}
+
+.icon {
+  width: 40px;
+}
+
+.score {
+  font-weight: bolder;
+  font-size: 1.75rem;
+}
+
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
