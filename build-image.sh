0e25b2c2c6d17547f859309513f50ef6a3ba6fbd
julioct99 <jcasallot01@gmail.com>
2020-10-09 20:19:37 +0200

    Change build-image script

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,9 @@
 APP="mediafinder"
+PROFILE="julioct99"
 
 docker image build -t $APP . 
-docker image tag $APP julioct99/$APP
+
+docker image tag $APP $PROFILE/$APP
+docker image tag $APP $PROFILE/$APP:build
+
+docker image push $PROFILE/$APP:build
